0af377120eed53dc50a59afde7ffe44e8b13b3a4
Shane Vandegrift <vandesm14@gmail.com>
2023-12-18 16:05:41 -0500

    echo cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-  println!("Hello, world!");
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
+
+fn main() -> Result<()> {
+  // `()` can be used when no completer is required
+  let mut rl = DefaultEditor::new()?;
+
+  loop {
+    let readline = rl.readline(">> ");
+    match readline {
+      Ok(line) => {
+        rl.add_history_entry(line.as_str());
+        println!("Line: {}", line);
+      }
+      Err(ReadlineError::Interrupted) => {
+        println!("CTRL-C");
+        break;
+      }
+      Err(ReadlineError::Eof) => {
+        println!("CTRL-D");
+        break;
+      }
+      Err(err) => {
+        println!("Error: {:?}", err);
+        break;
+      }
+    }
+  }
+
+  Ok(())
 }
