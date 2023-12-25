21453b617626adba5e73f4d13e3a8d43acc197f0
Shane Vandegrift <vandesm14@gmail.com>
2023-12-25 16:09:25 -0500

    fix list/reverse and update the test to be more explicit

--- examples/reverse.rs
+++ examples/reverse.rs
@@ -19,10 +19,17 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+  use stack::Expr;
+
   use super::*;
 
   #[test]
   fn reverse() {
-    run().unwrap();
+    let mut result = run().unwrap();
+    let expected = result
+      .context
+      .intern("Hello, World!".chars().rev().collect::<String>());
+
+    assert_eq!(result.stack, vec![Expr::String(expected)]);
   }
 }
