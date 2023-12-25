82e8c392e31606a87035758369d8e159f382d05f
leonskidev <leonskidev@pm.me>
2023-12-25 15:31:38 +0000

    intern all intrinsics

--- src/lib.rs
+++ src/lib.rs
@@ -21,37 +21,76 @@ impl Context {
   pub fn new() -> Self {
     let mut interner = Rodeo::new();
 
-    // TODO: Add all of the intrinsics.
-    interner.get_or_intern_static("");
-
+      // Arithmetic
     interner.get_or_intern_static("+");
     interner.get_or_intern_static("-");
     interner.get_or_intern_static("*");
     interner.get_or_intern_static("/");
     interner.get_or_intern_static("%");
+
+    // Comparison
     interner.get_or_intern_static("=");
     interner.get_or_intern_static("!=");
     interner.get_or_intern_static(">");
     interner.get_or_intern_static("<");
-
-    interner.get_or_intern_static("'");
-
-    interner.get_or_intern_static("(");
-    interner.get_or_intern_static(")");
-    interner.get_or_intern_static("{");
-    interner.get_or_intern_static("}");
-    interner.get_or_intern_static("[");
-    interner.get_or_intern_static("]");
-
-    interner.get_or_intern_static("and");
     interner.get_or_intern_static("or");
+    interner.get_or_intern_static("and");
 
+    // Code/IO
+    interner.get_or_intern_static("parse");
+    interner.get_or_intern_static("read-file");
+    interner.get_or_intern_static("syscall0");
+    interner.get_or_intern_static("syscall1");
+    interner.get_or_intern_static("syscall2");
+    interner.get_or_intern_static("syscall3");
+    interner.get_or_intern_static("syscall4");
+    interner.get_or_intern_static("syscall5");
+    interner.get_or_intern_static("syscall6");
+
+    // List
+    interner.get_or_intern_static("explode");
+    interner.get_or_intern_static("len");
+    interner.get_or_intern_static("nth");
+    interner.get_or_intern_static("join");
+    interner.get_or_intern_static("insert");
+    interner.get_or_intern_static("list-pop");
+    interner.get_or_intern_static("list-shift");
+    interner.get_or_intern_static("concat");
+    interner.get_or_intern_static("unwrap");
+
+    // Control Flow
+    interner.get_or_intern_static("ifelse");
+    interner.get_or_intern_static("if");
+    interner.get_or_intern_static("while");
+    interner.get_or_intern_static("halt");
+
+    // Scope
+    interner.get_or_intern_static("set");
+    interner.get_or_intern_static("get");
+    interner.get_or_intern_static("unset");
+
+    // Stack
+    interner.get_or_intern_static("collect");
+    interner.get_or_intern_static("clear");
     interner.get_or_intern_static("pop");
     interner.get_or_intern_static("dup");
     interner.get_or_intern_static("swap");
     interner.get_or_intern_static("rot");
 
-    interner.get_or_intern_static("fn");
+    // Functions/Data
+    interner.get_or_intern_static("call");
+    interner.get_or_intern_static("call_native");
+    interner.get_or_intern_static("lazy");
+
+    // Type
+    interner.get_or_intern_static("toboolean");
+    interner.get_or_intern_static("tointeger");
+    interner.get_or_intern_static("tofloat");
+    interner.get_or_intern_static("topointer");
+    interner.get_or_intern_static("tolist");
+    interner.get_or_intern_static("tostring");
+    interner.get_or_intern_static("tocall");
+    interner.get_or_intern_static("typeof");
 
     Self { interner }
   }
