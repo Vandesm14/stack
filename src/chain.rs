16d23aa7180e77a356972b1fb401d87c1d06cb88
Shane Vandegrift <vandesm14@gmail.com>
2024-02-10 10:16:07 -0500

    remove some comments

--- src/chain.rs
+++ src/chain.rs
@@ -1,73 +1,6 @@
 use core::cell::RefCell;
 use std::{borrow::BorrowMut, rc::Rc};
 
-// TODO: Look into a more efficient way to implement this.
-
-// #[derive(Debug, Clone)]
-// pub enum Chain<T> {
-//   Root(Rc<RefCell<T>>),
-//   Link(Rc<RefCell<Self>>),
-// }
-
-// impl<T> PartialEq for Chain<T>
-// where
-//   T: PartialEq,
-// {
-//   fn eq(&self, other: &Self) -> bool {
-//     RefCell::borrow(&self.root()).eq(&RefCell::borrow(&other.root()))
-//   }
-// }
-
-// impl<T> Chain<T> {
-//   #[inline]
-//   pub fn new(value: T) -> Self {
-//     Self::Root(Rc::new(RefCell::new(value)))
-//   }
-
-//   pub fn root(&self) -> Rc<RefCell<T>> {
-//     match self {
-//       Self::Root(root) => root.clone(),
-//       Self::Link(link) => RefCell::borrow(link).root(),
-//     }
-//   }
-
-//   pub fn link(&self) -> Self {
-//     match self {
-//       Self::Root(root) => {
-//         Self::Link(Rc::new(RefCell::new(Self::Root(root.clone()))))
-//       }
-//       Self::Link(link) => {
-//         Self::Link(Rc::new(RefCell::new(Self::Link(link.clone()))))
-//       }
-//     }
-//   }
-
-//   pub fn unlink_with<F>(&self, f: F)
-//   where
-//     F: FnOnce(&Self) -> T,
-//   {
-//     match self {
-//       Self::Root(_) => {}
-//       Self::Link(link) => *RefCell::borrow_mut(link) = Self::new(f(self)),
-//     }
-//   }
-// }
-
-// impl<T> Chain<T>
-// where
-//   T: Clone,
-// {
-//   #[inline]
-//   pub fn unlink(&self) {
-//     self.unlink_with(|chain| RefCell::borrow(&chain.root()).clone())
-//   }
-
-//   #[inline]
-//   pub fn val(&self) -> T {
-//     RefCell::borrow(&self.root()).clone()
-//   }
-// }
-
 #[derive(Debug, PartialEq, Clone)]
 pub struct Chain<T> {
   value: Rc<RefCell<T>>,
@@ -136,7 +69,6 @@ where
   }
 }
 
-// TODO: Add tests
 #[cfg(test)]
 mod tests {
   use super::*;
