109a061416fc7eb6a9c71e4711eca1152e3223d6
leonskidev <leonskidev@pm.me>
2024-02-10 13:54:44 +0000

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod chain;
 mod evaluator;
 mod expr;
 mod interner;
@@ -5,12 +6,11 @@ mod intrinsics;
 mod lexer;
 mod parser;
 mod scope;
-mod chain;
 
+pub use chain::*;
 pub use evaluator::*;
 pub use expr::*;
 pub use intrinsics::*;
 pub use lexer::*;
 pub use parser::*;
 pub use scope::*;
-pub use chain::*;
