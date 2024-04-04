d36a58bf089820117146603d62bdc36e6049c2fd
Shane Vandegrift <vandesm14@gmail.com>
2024-04-04 16:01:32 -0400

    update map module

--- src/module/map.rs
+++ src/module/map.rs
@@ -3,16 +3,17 @@ use std::{collections::HashMap, rc::Rc};
 
 use lasso::Spur;
 
-use crate::{interner::interner, EvalError, Expr, Program};
+use crate::{interner::interner, Ast, AstIndex, EvalError, Expr, Program};
 
 pub fn module(program: &mut Program) -> Result<(), EvalError> {
   program.funcs.insert(
     interner().get_or_intern_static("map/new"),
     |program, _| {
-      program
-        .push(Expr::UserData(Rc::new(RefCell::new(
-          HashMap::<Spur, Expr>::new(),
-        ))))?;
+      program.push_expr(Expr::UserData(Rc::new(RefCell::new(HashMap::<
+        Spur,
+        Expr,
+      >::new(
+      )))))?;
       Ok(())
     },
   );
@@ -20,18 +21,18 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
   program.funcs.insert(
     interner().get_or_intern_static("map/insert"),
     |program, trace_expr| {
-      let key = program.pop(trace_expr)?;
+      let key = program.pop_expr(trace_expr)?;
       let item = program.pop(trace_expr)?;
-      let map = program.pop(trace_expr)?;
+      let (map, map_index) = program.pop_with_index(trace_expr)?;
 
-      program.push(map.clone())?;
+      program.push(map_index)?;
 
       match map {
         Expr::UserData(map) => {
-          match map.borrow_mut().downcast_mut::<HashMap<Spur, Expr>>() {
+          match map.borrow_mut().downcast_mut::<HashMap<Spur, AstIndex>>() {
             Some(map) => match key {
               Expr::Call(key) | Expr::String(key) => {
-                map.insert(key, item);
+                map.insert(*key, item);
               }
               found => {
                 return Err(EvalError {
@@ -39,7 +40,7 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
                   expr: trace_expr.clone(),
                   message: format!(
                     "expected call or string, found {}",
-                    found.type_of()
+                    found.type_of(&program.ast)
                   ),
                 })
               }
@@ -57,7 +58,10 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
           return Err(EvalError {
             program: program.clone(),
             expr: trace_expr.clone(),
-            message: format!("expected userdata, found {}", found.type_of()),
+            message: format!(
+              "expected userdata, found {}",
+              found.type_of(&program.ast)
+            ),
           })
         }
       }
@@ -69,14 +73,14 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
   program.funcs.insert(
     interner().get_or_intern_static("map/remove"),
     |program, trace_expr| {
-      let key = program.pop(trace_expr)?;
-      let map = program.pop(trace_expr)?;
+      let key = program.pop_expr(trace_expr)?;
+      let (map, map_index) = program.pop_with_index(trace_expr)?;
 
-      program.push(map.clone())?;
+      program.push(map_index)?;
 
       match map {
         Expr::UserData(map) => {
-          match map.borrow_mut().downcast_mut::<HashMap<Spur, Expr>>() {
+          match map.borrow_mut().downcast_mut::<HashMap<Spur, AstIndex>>() {
             Some(map) => match key {
               Expr::Call(ref key) | Expr::String(ref key) => {
                 map.remove(key);
@@ -87,7 +91,7 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
                   expr: trace_expr.clone(),
                   message: format!(
                     "expected call or string, found {}",
-                    found.type_of()
+                    found.type_of(&program.ast)
                   ),
                 })
               }
@@ -105,7 +109,10 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
           return Err(EvalError {
             program: program.clone(),
             expr: trace_expr.clone(),
-            message: format!("expected userdata, found {}", found.type_of()),
+            message: format!(
+              "expected userdata, found {}",
+              found.type_of(&program.ast)
+            ),
           })
         }
       }
@@ -117,17 +124,17 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
   program.funcs.insert(
     interner().get_or_intern_static("map/get"),
     |program, trace_expr| {
-      let key = program.pop(trace_expr)?;
-      let map = program.pop(trace_expr)?;
+      let key = program.pop_expr(trace_expr)?;
+      let (map, map_index) = program.pop_with_index(trace_expr)?;
 
-      program.push(map.clone())?;
+      program.push(map_index)?;
 
       match map {
         Expr::UserData(map) => {
-          match map.borrow().downcast_ref::<HashMap<Spur, Expr>>() {
+          match map.borrow().downcast_ref::<HashMap<Spur, AstIndex>>() {
             Some(map) => match key {
               Expr::Call(ref key) | Expr::String(ref key) => {
-                program.push(map.get(key).cloned().unwrap_or(Expr::Nil))?;
+                program.push(map.get(key).cloned().unwrap_or(Ast::NIL))?;
               }
               found => {
                 return Err(EvalError {
@@ -135,7 +142,7 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
                   expr: trace_expr.clone(),
                   message: format!(
                     "expected call or string, found {}",
-                    found.type_of()
+                    found.type_of(&program.ast)
                   ),
                 })
               }
@@ -153,7 +160,10 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
           return Err(EvalError {
             program: program.clone(),
             expr: trace_expr.clone(),
-            message: format!("expected userdata, found {}", found.type_of()),
+            message: format!(
+              "expected userdata, found {}",
+              found.type_of(&program.ast)
+            ),
           })
         }
       }
