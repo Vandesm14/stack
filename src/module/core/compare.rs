40b7d208d71a61cb6cbd7f8afdcbe928202660aa
Shane Vandegrift <vandesm14@gmail.com>
2024-04-04 14:15:15 -0400

    update compare module

--- src/module/core/compare.rs
+++ src/module/core/compare.rs
@@ -4,60 +4,72 @@ pub fn module(program: &mut Program) -> Result<(), EvalError> {
   program.funcs.insert(
     interner().get_or_intern_static("="),
     |program, trace_expr| {
-      let rhs = program.pop(trace_expr)?;
-      let lhs = program.pop(trace_expr)?;
+      let rhs = program.pop_expr(trace_expr)?;
+      let lhs = program.pop_expr(trace_expr)?;
 
-      program.push(Expr::Boolean(lhs == rhs))
+      program.push_expr(Expr::Boolean(lhs == rhs))?;
+
+      Ok(())
     },
   );
 
   program.funcs.insert(
     interner().get_or_intern_static("!="),
     |program, trace_expr| {
-      let rhs = program.pop(trace_expr)?;
-      let lhs = program.pop(trace_expr)?;
+      let rhs = program.pop_expr(trace_expr)?;
+      let lhs = program.pop_expr(trace_expr)?;
 
-      program.push(Expr::Boolean(lhs != rhs))
+      program.push_expr(Expr::Boolean(lhs != rhs))?;
+
+      Ok(())
     },
   );
 
   program.funcs.insert(
     interner().get_or_intern_static("<"),
     |program, trace_expr| {
-      let rhs = program.pop(trace_expr)?;
-      let lhs = program.pop(trace_expr)?;
+      let rhs = program.pop_expr(trace_expr)?;
+      let lhs = program.pop_expr(trace_expr)?;
 
-      program.push(Expr::Boolean(lhs < rhs))
+      program.push_expr(Expr::Boolean(lhs < rhs))?;
+
+      Ok(())
     },
   );
 
   program.funcs.insert(
     interner().get_or_intern_static(">"),
     |program, trace_expr| {
-      let rhs = program.pop(trace_expr)?;
-      let lhs = program.pop(trace_expr)?;
+      let rhs = program.pop_expr(trace_expr)?;
+      let lhs = program.pop_expr(trace_expr)?;
 
-      program.push(Expr::Boolean(lhs > rhs))
+      program.push_expr(Expr::Boolean(lhs > rhs))?;
+
+      Ok(())
     },
   );
 
   program.funcs.insert(
     interner().get_or_intern_static("<="),
     |program, trace_expr| {
-      let rhs = program.pop(trace_expr)?;
-      let lhs = program.pop(trace_expr)?;
+      let rhs = program.pop_expr(trace_expr)?;
+      let lhs = program.pop_expr(trace_expr)?;
 
-      program.push(Expr::Boolean(lhs <= rhs))
+      program.push_expr(Expr::Boolean(lhs <= rhs))?;
+
+      Ok(())
     },
   );
 
   program.funcs.insert(
     interner().get_or_intern_static(">="),
     |program, trace_expr| {
-      let rhs = program.pop(trace_expr)?;
-      let lhs = program.pop(trace_expr)?;
+      let rhs = program.pop_expr(trace_expr)?;
+      let lhs = program.pop_expr(trace_expr)?;
 
-      program.push(Expr::Boolean(lhs >= rhs))
+      program.push_expr(Expr::Boolean(lhs >= rhs))?;
+
+      Ok(())
     },
   );
 
