61a0c23539c754dcb1545154705dd472926f76ad
Shane Vandegrift <vandesm14@gmail.com>
2024-04-09 10:39:36 -0400

    move core.stack into the std folder

--- src/module/core/core.rs
+++ src/module/core/core.rs
@@ -3,8 +3,7 @@ use crate::{EvalError, Program};
 // TODO: Split `core` into `list` and `module` modules
 
 pub fn module(program: &mut Program) -> Result<(), EvalError> {
-  let core_lib = include_str!("./core.stack");
-  program.eval_string(core_lib)?;
+  program.eval_file("std/core.stack")?;
 
   Ok(())
 }
