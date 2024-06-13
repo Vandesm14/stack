a81ef9c3a6097fa51c9dd32206cef1e33dec65c0
Shane <vandesm14@gmail.com>
2024-06-12 20:07:21 -0400

    add scope:is-loaded method (#50)

--- stack-std/src/scope.rs
+++ stack-std/src/scope.rs
@@ -16,17 +16,29 @@ pub fn module() -> Module {
           ExprKind::Symbol(ref x) => {
             if Intrinsic::from_str(x.as_str()).is_ok() {
               context.stack_push(ExprKind::String("intrinsic".into()).into())
-            } else if engine
-              .module(&Symbol::new(
-                x.as_str().split(':').next().unwrap_or_default().into(),
-              ))
-              .is_some()
-            {
-              context.stack_push(ExprKind::String("module".into()).into())
             } else if context.let_get(*x).is_some() {
               context.stack_push(ExprKind::String("let".into()).into())
             } else if context.scope_item(*x).is_some() {
               context.stack_push(ExprKind::String("scope".into()).into())
+            } else if engine.module(x).is_some() {
+              context.stack_push(ExprKind::String("module".into()).into())
+            } else if x.as_str().contains(':') {
+              if let Some(module) = engine.module(&Symbol::new(
+                x.as_str().split(':').next().unwrap_or_default().into(),
+              )) {
+                if module
+                  .func(Symbol::new(
+                    x.as_str().split(':').nth(1).unwrap_or_default().into(),
+                  ))
+                  .is_some()
+                {
+                  context.stack_push(ExprKind::String("module".into()).into())
+                } else {
+                  context.stack_push(ExprKind::Nil.into())
+                }
+              } else {
+                context.stack_push(ExprKind::Nil.into())
+              }
             } else {
               context.stack_push(ExprKind::Nil.into())
             }
@@ -59,6 +71,19 @@ pub fn module() -> Module {
         context
           .stack_push(ExprKind::List(items).into())
           .map(|_| context)
+      }),
+    )
+    .add_func(
+      Symbol::from_ref("is-loaded"),
+      Arc::new(|engine, mut context, expr| {
+        let symbol = context.stack_pop(&expr)?;
+
+        match symbol.kind {
+          ExprKind::Symbol(ref x) => context
+            .stack_push(ExprKind::Boolean(engine.module(x).is_some()).into()),
+          _ => context.stack_push(ExprKind::Nil.into()),
+        }
+        .map(|_| context)
       }),
     );
 
