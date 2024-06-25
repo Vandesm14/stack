6a6a8b00f93e265bf1842294e37de12d31f1882a
Shane <vandesm14@gmail.com>
2024-06-25 12:49:30 -0400

    add more tests

--- stack-core/src/tests.rs
+++ stack-core/src/tests.rs
@@ -74,6 +74,26 @@ mod lispy {
   }
 
   #[test]
+  fn less_arity_pops() {
+    let source = Source::new("", "2 (- 10)");
+    let mut lexer = Lexer::new(source);
+    let exprs = crate::parser::parse(&mut lexer).unwrap();
+
+    let engine = Engine::new();
+    let mut context = Context::new().with_stack_capacity(32);
+    context = engine.run(context, exprs).unwrap();
+
+    assert_eq!(
+      context
+        .stack()
+        .iter()
+        .map(|expr| &expr.kind)
+        .collect::<Vec<_>>(),
+      vec![&ExprKind::Integer(8)]
+    )
+  }
+
+  #[test]
   fn underscores_order() {
     let source = Source::new("", "10 (- _ 2)");
     let mut lexer = Lexer::new(source);
@@ -136,6 +156,87 @@ mod lispy {
   #[test]
   fn insert_works() {
     let source = Source::new("", "(insert {} \"key\" \"value\")");
+    let mut lexer = Lexer::new(source);
+    let exprs = crate::parser::parse(&mut lexer).unwrap();
+
+    let engine = Engine::new();
+    let mut context = Context::new().with_stack_capacity(32);
+    context = engine.run(context, exprs).unwrap();
+
+    assert_eq!(
+      context
+        .stack()
+        .to_vec()
+        .iter_mut()
+        .map(|expr| {
+          expr.recursively_strip_info();
+          &expr.kind
+        })
+        .collect::<Vec<_>>(),
+      vec![&ExprKind::Record(HashMap::from_iter(vec![(
+        Symbol::new("key".to_owned().into()),
+        ExprKind::String("value".into()).into()
+      )]))]
+    )
+  }
+
+  #[test]
+  fn insert_works_outer_ordered() {
+    let source = Source::new("", "\"value\" (insert {} \"key\" _)");
+    let mut lexer = Lexer::new(source);
+    let exprs = crate::parser::parse(&mut lexer).unwrap();
+
+    let engine = Engine::new();
+    let mut context = Context::new().with_stack_capacity(32);
+    context = engine.run(context, exprs).unwrap();
+
+    assert_eq!(
+      context
+        .stack()
+        .to_vec()
+        .iter_mut()
+        .map(|expr| {
+          expr.recursively_strip_info();
+          &expr.kind
+        })
+        .collect::<Vec<_>>(),
+      vec![&ExprKind::Record(HashMap::from_iter(vec![(
+        Symbol::new("key".to_owned().into()),
+        ExprKind::String("value".into()).into()
+      )]))]
+    )
+  }
+
+  #[test]
+  fn insert_works_ordered() {
+    let source = Source::new("", "\"value\" {} (insert _ \"key\" _)");
+    let mut lexer = Lexer::new(source);
+    let exprs = crate::parser::parse(&mut lexer).unwrap();
+
+    let engine = Engine::new();
+    let mut context = Context::new().with_stack_capacity(32);
+    context = engine.run(context, exprs).unwrap();
+
+    assert_eq!(
+      context
+        .stack()
+        .to_vec()
+        .iter_mut()
+        .map(|expr| {
+          expr.recursively_strip_info();
+          &expr.kind
+        })
+        .collect::<Vec<_>>(),
+      vec![&ExprKind::Record(HashMap::from_iter(vec![(
+        Symbol::new("key".to_owned().into()),
+        ExprKind::String("value".into()).into()
+      )]))]
+    )
+  }
+
+  #[test]
+  fn insert_works_inner_ordered() {
+    let source = Source::new("", "{} (insert _ \"key\" \"value\")");
     let mut lexer = Lexer::new(source);
     let exprs = crate::parser::parse(&mut lexer).unwrap();
 
